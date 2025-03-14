7ec5eb3169dea15aaade0cdf7b75c3ff2eabec9c
Ngô Minh Thuận <111260265+thuanthichlaptrinh@users.noreply.github.com>
2025-03-14 17:54:13 +0700

    Khắc Phục Vấn Đề Nhiều File Index.js

---
+++ src/components/Button/Button.js
@@ -0,0 +1,74 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rouded = false,
+    disable = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps, // sẽ chứa các props ví dụ: target...
+    };
+
+    // Nếu btn có type disable thì không cho click
+    if (disable) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    // khi nào props đc truyền trong {} thì nó sẽ được thêm vào className
+    const classes = cx('wrapper', {
+        [className]: className, // Khi có className thì sẽ lấy giá trị của className làm key
+        primary,
+        outline,
+        text,
+        disable,
+        rouded,
+        small,
+        large,
+    });
+
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+Button.propTypes = {
+    children: PropTypes.node,
+};
+
+export default Button;
---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,111 @@
+import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import HeadLessTippy from '@tippyjs/react/headless';
+import AccountItem from '~/components/AccountItem/AccountItem';
+import classNames from 'classnames/bind';
+import styles from './Search.module.scss';
+
+import * as searchServices from '~/services/searchService';
+import { SearchIcon } from '~/components/Icons';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import { useDebouce } from '~/hooks';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debouced = useDebouce(searchValue, 500);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debouced.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debouced, 'less');
+            setSearchResult(result);
+
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debouced]);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    // Xử lý không cho người dùng nhập dấu cách khi bắt đầu
+    const handleChange = (e) => {
+        const search = e.target.value;
+
+        // Nếu không bắt đầu bằng dấu cách
+        if (!search.startsWith(' ')) {
+            setSearchValue(search);
+        }
+    };
+
+    return (
+        // sử dụng thẻ div ôm Tippy dể không bị warning
+        <div>
+            <HeadLessTippy
+                visible={showResult && searchResult.length > 0} // Có kết quả thì mới hiện tippy
+                interactive // Nhấn được vào tippy
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex={-1} {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {searchResult.map((result) => (
+                                <AccountItem key={result.id} data={result} />
+                            ))}
+                        </PopperWrapper>
+                    </div>
+                )}
+                onClickOutside={handleHideResult}
+            >
+                <div className={cx('search')}>
+                    <input
+                        ref={inputRef}
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        onChange={handleChange}
+                        onFocus={() => setShowResult(true)} // khi focus thì bật tippy lên
+                    />
+
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear-btn')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+
+                    {loading && <FontAwesomeIcon className={cx('loading-icon')} icon={faSpinner} />}
+
+                    <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+                        <SearchIcon />
+                    </button>
+                </div>
+            </HeadLessTippy>
+        </div>
+    );
+}
+
+export default Search;
+
+// onMouseDown={(e) => e.preventDefault()}: bỏ đi border khi nhấn vào btn search
