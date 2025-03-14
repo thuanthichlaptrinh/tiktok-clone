741fa4242307bb393b9c2a276702329bc24e1fe5
Ngô Minh Thuận <111260265+thuanthichlaptrinh@users.noreply.github.com>
2025-03-14 22:53:29 +0700

    Xây dựng UI phần Sidebar p1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,12 +10,14 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 // Sử dụng cho những route không cần đăng nhập nhưng vẫn xem được
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
     { path: config.routes.profile, component: Profile },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
 ];
